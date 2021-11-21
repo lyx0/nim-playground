c5d0943f19a8fdbc224329de41ac48b0f227e946
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-21 14:11:42 +0100

    init commit

---
+++ basics/README.md
@@ -0,0 +1,5 @@
+# Basics
+
+Code snippets while going through [nim-basics](https://narimiran.github.io/nim-basics/)
+
+
---
+++ basics/naming.nim
@@ -0,0 +1,15 @@
+var a: int
+var b = 7
+
+var 
+  c = -11
+  d = "Hello"
+  e = '!'
+
+var f = 7
+
+f = -3
+f = 19
+# f = "Hello" # error, type mismatch
+
+echo(a, b, c, d, e)
